0dcde3f9d67c4c4810e09ff05802594e8d27e9dc
UpmostDev <jordynrichmond@gmail.com>
2026-04-01 16:03:27 +0500

    Initial Commit.

---
+++ U4_MemoryGame/U4_MemoryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  U4_MemoryGameApp.swift
+//  U4_MemoryGame
+//
+//  Created by Jordyn on 01/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct U4_MemoryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
